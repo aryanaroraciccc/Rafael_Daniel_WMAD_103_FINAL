0cc14a608f7ed0e0911bae430b795c5e0a3d70b1
Daniel González Briseño <qbixmex@gmail.com>
2023-07-20 17:03:13 -0700

    added-mobile-navigation

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
-console.log("JS Started");
+const hamburger = document.getElementById('hamburger-icon');
+const navigation = document.getElementById('buttons_top');
 
+hamburger.addEventListener('click', (event) => {
+  navigation.classList.toggle('navbar-display');
+});
